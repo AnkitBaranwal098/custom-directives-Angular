7404cc21dd66a57a1ce5b7f184f7ab13b16b6831
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-10-28 00:36:23 +0530

    using Renderer2 to safely access Dom elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
