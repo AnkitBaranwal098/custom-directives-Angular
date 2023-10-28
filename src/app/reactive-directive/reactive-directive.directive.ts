906709840d25e231d230101af1f71ed5d3c5e1cc
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-10-28 18:26:38 +0530

    Demo of HostBinding decorator

--- src/app/reactive-directive/reactive-directive.directive.ts
+++ src/app/reactive-directive/reactive-directive.directive.ts
@@ -1,18 +1,25 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appReactiveDirective]'
 })
 export class ReactiveDirectiveDirective {
 
+  // We simply pass the css property we want to change in camel case as dom don't understand -.So with this we are telling Angular is on the element when this direcitve sits access the style property and then it's sub property which is the backgroundColor property
+
+  // We sets it's initial value so that wee do not get any error
+  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
+
   constructor(private elRef:ElementRef, private renderer:Renderer2) { }
 
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color","orange");
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color","orange");
+    this.backgroundColor = "orange";
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color","transparent");
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color","transparent");
+    this.backgroundColor = "transparent"
   }
 
 }
