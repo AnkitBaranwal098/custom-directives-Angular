1fd3a5f5e39997b3bf401b2e5ac5e0225a6ca0a8
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-10-28 19:02:16 +0530

    Binding to directive properties using custom property binding

--- src/app/reactive-directive/reactive-directive.directive.ts
+++ src/app/reactive-directive/reactive-directive.directive.ts
@@ -8,7 +8,7 @@ export class ReactiveDirectiveDirective {
   // We simply pass the css property we want to change in camel case as dom don't understand -.So with this we are telling Angular is on the element when this direcitve sits access the style property and then it's sub property which is the backgroundColor property
 
   // We sets it's initial value so that wee do not get any error
-  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef:ElementRef, private renderer:Renderer2) { }
 
